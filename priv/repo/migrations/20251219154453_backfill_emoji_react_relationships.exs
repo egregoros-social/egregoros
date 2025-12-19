57c0b8d271ad7c28492731238f7cd4d466491b62
Lain Soykaf <lain@lain.com>
2025-12-19 19:45:52 +0400

    Backfill EmojiReact relationships

---
+++ priv/repo/migrations/20251219154453_backfill_emoji_react_relationships.exs
@@ -0,0 +1,25 @@
+defmodule PleromaRedux.Repo.Migrations.BackfillEmojiReactRelationships do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+    INSERT INTO relationships (type, actor, object, activity_ap_id, inserted_at, updated_at)
+    SELECT 'EmojiReact:' || emoji, actor, object, ap_id, NOW(), NOW()
+    FROM (
+      SELECT DISTINCT ON (actor, object, (data->>'content')) actor, object, ap_id, (data->>'content') AS emoji
+      FROM objects
+      WHERE type = 'EmojiReact'
+        AND (data->>'content') IS NOT NULL
+        AND (data->>'content') <> ''
+      ORDER BY actor, object, (data->>'content'), inserted_at DESC NULLS LAST, id DESC
+    ) latest
+    ON CONFLICT (type, actor, object) DO UPDATE
+      SET activity_ap_id = EXCLUDED.activity_ap_id,
+          updated_at = EXCLUDED.updated_at;
+    """)
+  end
+
+  def down do
+    :ok
+  end
+end
