125546d75ad4cb3207a7fed4f74d63569f23b098
Lain Soykaf <lain@lain.com>
2025-12-19 19:12:23 +0400

    Backfill follow relationships from existing objects

---
+++ priv/repo/migrations/20251219151118_backfill_follow_relationships.exs
@@ -0,0 +1,23 @@
+defmodule PleromaRedux.Repo.Migrations.BackfillFollowRelationships do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+    INSERT INTO relationships (type, actor, object, activity_ap_id, inserted_at, updated_at)
+    SELECT 'Follow', actor, object, ap_id, NOW(), NOW()
+    FROM (
+      SELECT DISTINCT ON (actor, object) actor, object, ap_id
+      FROM objects
+      WHERE type = 'Follow'
+      ORDER BY actor, object, inserted_at DESC NULLS LAST, id DESC
+    ) latest
+    ON CONFLICT (type, actor, object) DO UPDATE
+      SET activity_ap_id = EXCLUDED.activity_ap_id,
+          updated_at = EXCLUDED.updated_at;
+    """)
+  end
+
+  def down do
+    :ok
+  end
+end
